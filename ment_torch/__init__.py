27997e0c3adce87e4ac9e27388bd6e04eb21c0dc
austin-hoover <austin.michael.hoover@gmail.com>
2025-03-06 17:40:35 -0500

    Update __init__

--- ment_torch/__init__.py
+++ ment_torch/__init__.py
@@ -3,5 +3,13 @@ from .diag import *
 from .prior import *
 from .sim import *
 
+from .prior import GaussianPrior
+from .prior import InfiniteUniformPrior
+from .samp import GridSampler
+
 from . import dist
 from . import diag
+from . import prior
+from . import samp
+from . import sim
+from . import utils
