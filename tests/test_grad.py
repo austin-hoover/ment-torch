7e8e0cfcf54df6b91ccfdf7d6f9f5591f6c3ce1a
austin-hoover <austin.michael.hoover@gmail.com>
2025-03-18 23:18:53 -0400

    Add gradient test

--- tests/test_grad.py
+++ tests/test_grad.py
@@ -4,7 +4,7 @@ from ment_torch.samp.hmc import compute_gradients
 from ment_torch.samp.hmc import compute_gradients_loop
 
 
-def test_grad_sin():
+def test_grad_01():
 
     def func(x: torch.Tensor) -> torch.Tensor:
         return torch.sin(x[:, 0]) + torch.cos(x[:, 1])
@@ -19,3 +19,15 @@ def test_grad_sin():
     assert torch.all(g_true == g_calc)
     assert torch.all(g_true == g_calc_loop)
     assert torch.all(g_calc == g_calc_loop)
+
+
+def test_grad_02():
+
+    def func(x: torch.Tensor) -> torch.Tensor:
+        return torch.sin(torch.pi * x[:, 0]) - 2.0 * (x[:, 0]**2 + x[:, 1]**2 - 2.0) ** 2
+
+    for _ in range(10):
+        x = torch.randn((5, 2))
+        g_calc = compute_gradients(func, x)
+        g_calc_loop = compute_gradients_loop(func, x)
+        assert torch.all(g_calc == g_calc_loop)
